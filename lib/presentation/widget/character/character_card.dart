7c985e18a1c904a8c1903e3edcd1aa89938bab12
hasan-ciftci <se.hasanicftci@gmail.com>
2021-12-26 18:43:29 +0300

    Character card display image only option added.

--- lib/presentation/widget/character/character_card.dart
+++ lib/presentation/widget/character/character_card.dart
@@ -6,6 +6,7 @@ class CharacterCard extends StatelessWidget {
   final CharactersModel charactersModel;
   final double height;
   final double width;
+  final bool displayImageOnly;
 
   final GestureTapCallback? onTap;
 
@@ -15,6 +16,7 @@ class CharacterCard extends StatelessWidget {
     this.height = 350,
     this.width = double.infinity,
     this.onTap,
+    this.displayImageOnly = false,
   }) : super(key: key);
 
   @override
@@ -48,48 +50,49 @@ class CharacterCard extends StatelessWidget {
                   child: Icon(Icons.error, color: Colors.white),
                 ),
               ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Container(
-                width: MediaQuery.of(context).size.width,
-                margin: const EdgeInsets.all(16),
-                padding: const EdgeInsets.all(16),
-                decoration: BoxDecoration(
-                  color: charactersModel.hasCharacterNameAndDescription
-                      ? Colors.black54
-                      : null,
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    if (charactersModel.hasCharacterName)
-                      Text(
-                        charactersModel.name!,
-                        maxLines: 3,
-                        overflow: TextOverflow.ellipsis,
-                        textAlign: TextAlign.center,
-                        style: Theme.of(context)
-                            .textTheme
-                            .headline5!
-                            .copyWith(color: Colors.white),
-                      ),
-                    if (charactersModel.hasDescription)
-                      Text(
-                        charactersModel.description!,
-                        maxLines: 5,
-                        overflow: TextOverflow.ellipsis,
-                        style: Theme.of(context)
-                            .textTheme
-                            .bodyText1!
-                            .copyWith(color: Colors.white70),
-                      ),
-                  ],
+            if (!displayImageOnly)
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  width: MediaQuery.of(context).size.width,
+                  margin: const EdgeInsets.all(16),
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: charactersModel.hasCharacterNameAndDescription
+                        ? Colors.black54
+                        : null,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      if (charactersModel.hasCharacterName)
+                        Text(
+                          charactersModel.name!,
+                          maxLines: 3,
+                          overflow: TextOverflow.ellipsis,
+                          textAlign: TextAlign.center,
+                          style: Theme.of(context)
+                              .textTheme
+                              .headline5!
+                              .copyWith(color: Colors.white),
+                        ),
+                      if (charactersModel.hasDescription)
+                        Text(
+                          charactersModel.description!,
+                          maxLines: 5,
+                          overflow: TextOverflow.ellipsis,
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodyText1!
+                              .copyWith(color: Colors.white70),
+                        ),
+                    ],
+                  ),
                 ),
               ),
-            ),
           ],
         ),
       ),
