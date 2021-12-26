e1ce8acc7adecf7d0f7d0afb0b52e644d40951f1
hasan-ciftci <se.hasanicftci@gmail.com>
2021-12-26 11:14:26 +0300

    Retrofit character service created.

---
+++ lib/data/service/remote/character/character_service.dart
@@ -0,0 +1,14 @@
+import 'package:dio/dio.dart';
+import 'package:path_challenge/product/utils/enums/api_route_enums.dart';
+import 'package:retrofit/retrofit.dart';
+import 'package:path_challenge/presentation/home/model/network/characters_response_model.dart';
+
+part 'character_service.g.dart';
+
+@RestApi()
+abstract class CharacterService {
+  factory CharacterService(Dio dio, {String baseUrl}) = _CharacterService;
+
+  @GET(ApiRoutes.characters)
+  Future<HttpResponse<CharacterResponseModel>> create();
+}
