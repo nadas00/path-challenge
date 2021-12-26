4550b94ef2245d8bf447302b42825e60dc8df3d3
hasan-ciftci <se.hasanicftci@gmail.com>
2021-12-26 11:14:03 +0300

    Change routes due to static for retrofit requirements.

--- lib/product/utils/enums/api_route_enums.dart
+++ lib/product/utils/enums/api_route_enums.dart
@@ -1,12 +1,3 @@
-class ApiRoutes {
-  static ApiRoutes? _instance;
-
-  static ApiRoutes get instance {
-    _instance ??= ApiRoutes._();
-    return _instance!;
-  }
-
-  ApiRoutes._();
-
-  final String characters = "/characters";
+abstract class ApiRoutes {
+  static const String characters = "/characters";
 }
